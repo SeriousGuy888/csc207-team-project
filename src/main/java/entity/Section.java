10d068ca9ecc2b6d67162e1921a6ed80b03f03f5
billzonium trioxide <38299346+SeriousGuy888@users.noreply.github.com>
2025-11-06 15:25:27 -0500

    added skeleton implementation of Section

--- src/main/java/entity/Section.java
+++ src/main/java/entity/Section.java
@@ -1,4 +1,24 @@
 package entity;
 
+import java.util.Set;
+
 public class Section {
+    private CourseOffering courseOffering;
+    private String sectionCode;
+    private SectionType type;
+    private Set<ClassSession> classSessions;
+
+    /**
+     * @return Union of all time slots that are occupied by the sessions in this section,
+     *         in other words, the times at which this section has class.
+     */
+    public ClassHours getOccupiedTime() {
+        // todo: implement getOccupiedTime
+        throw new UnsupportedOperationException();
+    }
+}
+
+enum SectionType {
+    LECTURE,
+    TUTORIAL
 }
