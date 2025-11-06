f71bd8d4c913feb8a5507f2846afff36d9c9f161
billzonium trioxide <38299346+SeriousGuy888@users.noreply.github.com>
2025-11-06 15:30:31 -0500

    added sekelton implementation of ClassSession

--- src/main/java/entity/ClassSession.java
+++ src/main/java/entity/ClassSession.java
@@ -1,4 +1,22 @@
 package entity;
 
+/**
+ * Represents a session of a section.
+ * <p>
+ * This is needed because
+ * each section of a class might have class multiples a week,
+ * such as once on Monday, once on Tuesday, once on Thursday, and each
+ * session might be in a different location/time from the others.
+ */
 public class ClassSession {
+    // This could be replaced with a dedicated UoftLocation class in the future, if needed
+    // doing so might make it more convenient to convert between uoft locations and
+    // street addresses for walk time estimations.
+    private String location;
+
+    private ClassHours time; // must be contiguous timespan
+
+    public String getLocation() {
+        return location;
+    }
 }
