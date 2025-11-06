cbeebf04169a525fe305ad4603b6517de7a8902e
billzonium trioxide <38299346+SeriousGuy888@users.noreply.github.com>
2025-11-06 15:24:36 -0500

    added skeleton implementation of CourseOffering

---
+++ src/main/java/entity/CourseOffering.java
@@ -0,0 +1,9 @@
+package entity;
+
+import java.util.Set;
+
+public class CourseOffering {
+    private String courseCode;
+    private Set<Section> availableSections;
+    private String courseDescription;
+}
