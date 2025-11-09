0b19716957e01df341fc1c9b8d634a44ee3cfdc9
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2025-11-08 19:27:21 -0500

    implement getOccupiedTime for Section

--- src/main/java/entity/ClassSession.java
+++ src/main/java/entity/ClassSession.java
@@ -19,4 +19,8 @@ public class ClassSession {
     public String getLocation() {
         return location;
     }
+
+    public ClassHours getTime() {
+        return time;
+    }
 }
--- src/main/java/entity/Section.java
+++ src/main/java/entity/Section.java
@@ -1,20 +1,26 @@
 package entity;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Section {
     private CourseOffering courseOffering;
     private String sectionCode;
     private SectionType type;
-    private Set<ClassSession> classSessions;
+    private final Set<ClassSession> classSessions = new HashSet<>();
 
     /**
      * @return Union of all time slots that are occupied by the sessions in this section,
      *         in other words, the times at which this section has class.
      */
     public ClassHours getOccupiedTime() {
-        // todo: implement getOccupiedTime
-        throw new UnsupportedOperationException();
+        List<ClassHours> hoursList = classSessions
+                .stream()
+                .map(ClassSession::getTime)
+                .collect(Collectors.toList());
+        return ClassHours.union(hoursList);
     }
 }
 
