6a9ed44994de3ade2e67c0cde784f685833ddf70
billzonium trioxide <38299346+SeriousGuy888@users.noreply.github.com>
2025-11-10 13:13:50 -0500

    implemented getConflicts in quadratic time (bad and yucky)

--- src/main/java/entity/Timetable.java
+++ src/main/java/entity/Timetable.java
@@ -1,6 +1,8 @@
 package entity;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -37,7 +39,31 @@ public class Timetable {
     }
 
     public Set<Section> getConflicts() {
-        // todo: implement timetable conflict checking
-        throw new UnsupportedOperationException();
+        Set<Section> conflicts = new HashSet<>();
+
+        // I think this implementation is \Theta(n^2) :pensive:
+        // Probably not actually a problem though if we're not checking conflicts twenty times a second
+
+        // I think I know a more efficient implementation, but I don't want to implement it right now:
+        // - Create a list of "histogram bins", each bin being a set of sections.
+        // - Loop through the sections and add each section to each of the bins of the slots it occupies (n steps)
+        // - Loop through the time slots with more than 1 section and record them as conflicts (constant steps)
+
+        // For each pair of distinct sections,
+        // if their occupied times intersect, mark both as conflicting.
+        List<Section> sectionsList = new ArrayList<>(sections);
+        for (int i = 0; i < sectionsList.size(); i++) {
+            Section a = sectionsList.get(i);
+            for (int j = i + 1; j < sectionsList.size(); j++) {
+                Section b = sectionsList.get(j);
+                if (a.getOccupiedTime().doesIntersect(b.getOccupiedTime())) {
+                    conflicts.add(a);
+                    conflicts.add(b);
+                }
+            }
+        }
+
+
+        return conflicts;
     }
 }
