db9735baf33a122db247556b5bd57e68a1dac093
SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>
2025-11-16 02:09:30 -0500

    added CA business logic classes for Load Workbook

--- src/main/java/data_access/workbook_persistence/FileWorkbookDataAccessObject.java
+++ src/main/java/data_access/workbook_persistence/FileWorkbookDataAccessObject.java
@@ -1,15 +1,22 @@
 package data_access.workbook_persistence;
 
+import entity.Workbook;
+import use_case.load_workbook.LoadWorkbookDataAccessInterface;
 import use_case.save_workbook.SaveWorkbookDataAccessInterface;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-public class FileWorkbookDataAccessObject implements SaveWorkbookDataAccessInterface {
+public class FileWorkbookDataAccessObject implements SaveWorkbookDataAccessInterface, LoadWorkbookDataAccessInterface {
     @Override
     public void save(String serialisedWorkbook, Path destination) throws IOException {
         Files.createDirectories(destination.subpath(0, destination.getNameCount() - 1));
         Files.writeString(destination, serialisedWorkbook);
+    }
+
+    @Override
+    public String load(Path source) throws IOException {
+        return Files.readString(source);
     }
 }
