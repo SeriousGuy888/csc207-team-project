2d97b4361f47689cffab2f223dd2218538d710f1
iitsbardia <53990780+iitsbardia@users.noreply.github.com>
2025-11-17 21:42:07 -0500

    Update src/main/java/data_access/BuildingAddressDAO.java
    Co-authored-by: SeriousGuy888 <38299346+SeriousGuy888@users.noreply.github.com>

--- src/main/java/data_access/BuildingAddressDAO.java
+++ src/main/java/data_access/BuildingAddressDAO.java
@@ -79,7 +79,6 @@ public class BuildingAddressDAO implements AddressDataAccessInterface {
     }
 
     /**
-     * Fulfills the interface contract from BuildingAddressDataAccessInterface.
      * Returns an Optional instead of a raw String or null.
      */
     @Override
