caaef1aad9bf4ee44457a906451e5b43b424b858
kyhphoebe <kyhphoebe@gmail.com>
2025-11-18 03:29:17 -0500

    search panel

---
+++ src/main/java/GUI/SearchPanel.java
@@ -0,0 +1,11 @@
+package GUI;
+
+import javax.swing.*;
+
+public class SearchPanel {
+
+    private JButton filterButton;
+    private JButton searchButton;
+    private JTextField searchField;
+    private JPanel SearchPanel;
+}
