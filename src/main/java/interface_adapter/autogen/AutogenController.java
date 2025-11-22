1efe6b9775f9ba2ba66ec494220f58fef863157c
Abish Kulkarni <abishkulkarni@gmail.com>
2025-11-21 22:20:34 -0500

    Finished AutogenController

--- src/main/java/interface_adapter/autogen/AutogenController.java
+++ src/main/java/interface_adapter/autogen/AutogenController.java
@@ -1,10 +1,22 @@
 package interface_adapter.autogen;
 
-import entity.WeeklyOccupancy;
 import entity.CourseCode;
-import entity.Section;
 import use_case.autogen.AutogenInputBoundary;
-//import use_case.autogen.AutogenRequestModel;
+import use_case.autogen.AutogenInputData;
+import entity.Section;
+import entity.WeeklyOccupancy;
+
+import java.util.Set;
 
 public class AutogenController {
+    private final AutogenInputBoundary interactor;
+
+    public AutogenController(AutogenInputBoundary interactor){this.interactor=interactor;}
+
+    public void generate(Set<CourseCode> selectedCourses,
+                         Set<Section> lockedSections,
+                         WeeklyOccupancy blockedTimes){
+        AutogenInputData data = new AutogenInputData(selectedCourses,lockedSections,blockedTimes);
+        interactor.execute(data);
+    }
 }
