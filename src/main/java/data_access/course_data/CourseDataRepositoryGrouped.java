3fca3e04a305fc3b6f1bd18ccb7c936865096fd8
CarolineLyu <168619926+CarolineLyu@users.noreply.github.com>
2025-11-26 19:12:02 -0500

    renamed to take out underline for style consistency

--- src/main/java/data_access/course_data/CourseDataRepositoryGrouped.java
+++ src/main/java/data_access/course_data/CourseDataRepositoryGrouped.java
@@ -6,7 +6,7 @@ import entity.CourseOffering;
  * Implementors of this interface handle loading in and storing a library of course offerings that are available,
  * and users of this class may access course offerings from that library.
  */
-public interface CourseDataRepository_CourseCode {
+public interface CourseDataRepositoryGrouped {
     /**
      * @param courseCode The key under which the set of corresponding courses is supposedly stored.
      * @return The corresponding set of course offerings, if it exists. Otherwise, null.
