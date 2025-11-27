c799a3486ebf643c6232d63d842921e8890fc16b
Shadow <abishkulkarni@gmail.com>
2025-11-27 14:41:25 -0500

    Updated AutogenViewModel to correct format

--- src/main/java/interface_adapter/autogen/AutogenViewModel.java
+++ src/main/java/interface_adapter/autogen/AutogenViewModel.java
@@ -1,7 +1,5 @@
 package interface_adapter.autogen;
 import entity.Timetable;
-import entity.Section;
-import java.util.Set;
 
 public class AutogenViewModel {
     private Timetable generatedTimetable;
@@ -9,10 +7,22 @@ public class AutogenViewModel {
     private String error;
 
     /** Getters and Setters for variables **/
-    public Timetable getTimetable(){return generatedTimetable;}
-    public String getMessage(){return message;}
-    public String getError(){return error;}
-    public void setTimetable(Timetable generatedTimetable){this.generatedTimetable = generatedTimetable;}
-    public void setMessage(String message){this.message = message;}
-    public void setError(String error){this.error = error;}
+    public Timetable getTimetable(){
+        return generatedTimetable;
+    }
+    public String getMessage(){
+        return message;
+    }
+    public String getError(){
+        return error;
+    }
+    public void setTimetable(Timetable generatedTimetable){
+        this.generatedTimetable = generatedTimetable;
+    }
+    public void setMessage(String message){
+        this.message = message;
+    }
+    public void setError(String error){
+        this.error = error;
+    }
 }
