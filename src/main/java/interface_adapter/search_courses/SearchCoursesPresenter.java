386a1aa353a6b742e4fc5c7b4d09b62e8eb04bcb
CarolineLyu <168619926+CarolineLyu@users.noreply.github.com>
2025-11-28 13:05:46 -0500

    updated to reflect old viewmodel implementation

--- src/main/java/interface_adapter/search_courses/SearchCoursesPresenter.java
+++ src/main/java/interface_adapter/search_courses/SearchCoursesPresenter.java
@@ -1,26 +1,41 @@
 package interface_adapter.search_courses;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import entity.CourseOffering;
 
 import use_case.search_courses.SearchCoursesOutputBoundary;
 import use_case.search_courses.SearchCoursesOutputData;
 
 public class SearchCoursesPresenter implements SearchCoursesOutputBoundary {
+    private final SearchCoursesViewModel viewModel;
 
-    private String getRawCourseString(CourseOffering courseoffering) {
-         return courseoffering.toString();
+    public SearchCoursesPresenter(SearchCoursesViewModel viewModel) {
+        this.viewModel = viewModel;
+    }
+
+    private Set<String> getCourseOfferingStrings(SearchCoursesOutputData outputData) {
+        Set<String> courseofferingstrings = new HashSet<>();
+        for (CourseOffering courseoffering : outputData.getMatchedCourses()){
+            courseofferingstrings.add(courseoffering.toString());
+        }
+        return courseofferingstrings;
     }
 
     @Override
     public void prepareSuccessView(SearchCoursesOutputData outputData) {
-        for (CourseOffering courseoffering : outputData.getMatchedCourses()){
-            String CourseOfferingString = getRawCourseString(courseoffering);
-            System.out.println(CourseOfferingString);
-        }
+        Set<String> courseofferingstrings = getCourseOfferingStrings(outputData);
+
+        viewModel.setState(new SearchCoursesState(courseofferingstrings));
+
+        viewModel.fireStateChangeEvent();
     }
 
     @Override
     public void prepareFailView(String errorMessage) {
-        System.err.println(errorMessage);
+        viewModel.setState(new SearchCoursesState(errorMessage));
+        
+        viewModel.fireStateChangeEvent();
     }
 }
