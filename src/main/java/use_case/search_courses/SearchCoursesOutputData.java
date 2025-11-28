257c311382af1bcfa4f92ca1c8b11701484ac035
CarolineLyu <168619926+CarolineLyu@users.noreply.github.com>
2025-11-28 13:01:39 -0500

    added some missing imports

---
+++ src/main/java/use_case/search_courses/SearchCoursesOutputData.java
@@ -0,0 +1,15 @@
+package use_case.search_courses;
+
+import java.util.Set;
+
+public class SearchCoursesOutputData {
+    private final Set<CourseOffering> matchedCourses;
+
+    public SearchCoursesOutputData(Set<CourseOffering> matchedCourses) {
+        this.matchedCourses = matchedCourses;
+    }
+
+    public Set<CourseOffering> getMatchedCourses() {
+        return matchedCourses;
+    }
+}
