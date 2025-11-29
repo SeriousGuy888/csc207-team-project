ac13494fa78fe93f62ba69bc74494403e5d25a47
Abish Kulkarni <abishkulkarni@gmail.com>
2025-11-28 23:05:00 -0500

    Now Autogen also checks for semester clash

--- src/main/java/entity/constraints/TimeConflictConstraint.java
+++ src/main/java/entity/constraints/TimeConflictConstraint.java
@@ -16,7 +16,9 @@ public class TimeConflictConstraint implements Constraint {
         Section[] list = chosen.toArray(new Section[0]);
         for (int i = 0; i < list.length; i++) {
             for (int j = i + 1; j < list.length; j++) {
-                if (list[i].getOccupiedTime().doesIntersect(list[j].getOccupiedTime())) {
+                if (list[i].getMeetings().iterator().next().getSemester()
+                        == list[j].getMeetings().iterator().next().getSemester()
+                        && list[i].getOccupiedTime().doesIntersect(list[j].getOccupiedTime())) {
                     return false;
                 }
             }
