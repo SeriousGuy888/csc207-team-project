1c435abe21a518707eabe4cac28bb4fd53ddde14
CarolineLyu <168619926+CarolineLyu@users.noreply.github.com>
2025-11-29 02:30:14 -0500

    minor type error fix

--- src/main/java/data_access/AddSectionDataAccessObject.java
+++ src/main/java/data_access/AddSectionDataAccessObject.java
@@ -1,44 +1,36 @@
 package data_access;
 
+import data_access.workbook_instance.CurrentWorkbook;
+import data_access.course_data.CourseDataRepository;
 import entity.Section;
 import entity.Workbook;
 import entity.CourseOffering;
-
 import use_case.addsection.AddSectionDataAccessInterface;
-import course_data.CourseDataRepository;
 
-/**
- * Class to retrieve Section and Workbook data for the Add Section use case.
- * Relies on two shared DAOs: CourseDataRepository and the Workbook instance.
- * Assumes both are properly initialized at application start-up and will not change during runtime.
- */
 public class AddSectionDataAccessObject implements AddSectionDataAccessInterface {
-    private final Workbook workbook;
+    private final CurrentWorkbook currentWorkbook;
     private final CourseDataRepository courseDataRepository;
 
     public AddSectionDataAccessObject(CourseDataRepository courseDataRepository,
-                                      Workbook workbook) {
-        this.workbook = workbook;
+                                      CurrentWorkbook currentWorkbook) {
+        this.currentWorkbook = currentWorkbook;
         this.courseDataRepository = courseDataRepository;
     }
 
     @Override
     public Workbook getWorkbook() {
-        return workbook;
+        return currentWorkbook.getWorkbook();  // Always gets current reference
     }
 
-
     @Override
-    public Section getSectionToAdd(String courseOfferingIdentifier, String sectionName) throws IllegalArgumentException {
+    public Section getSectionToAdd(String courseOfferingIdentifier, String sectionName) {
         CourseOffering courseOfferingToAdd = courseDataRepository.getCourseOffering(courseOfferingIdentifier);
 
-        // sectionName is taken from our UI so should be in expected format
         for (Section section : courseOfferingToAdd.getAvailableSections()) {
             if (section.getName().equals(sectionName)) {
                 return section;
             }
         }
-        throw new IllegalArgumentException("Section " + sectionName + " not found in Course Offering "
-                + courseOfferingId);
+        return null;  // Section not found
     }
 }
