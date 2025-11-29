76e7b45bf39ee0951366c04e41c31493ac97d833
CarolineLyu <168619926+CarolineLyu@users.noreply.github.com>
2025-11-28 23:30:00 -0500

    update data access object to use shared workbook and datarepository

--- src/main/java/data_access/AddSectionDataAccessObject.java
+++ src/main/java/data_access/AddSectionDataAccessObject.java
@@ -5,5 +5,40 @@ import entity.Workbook;
 import entity.CourseOffering;
 
 import use_case.addsection.AddSectionDataAccessInterface;
+import course_data.CourseDataRepository;
 
-public class AddSectionDataAccessObject implements 
+/**
+ * Class to retrieve Section and Workbook data for the Add Section use case.
+ * Relies on two shared DAOs: CourseDataRepository and the Workbook instance.
+ * Assumes both are properly initialized at application start-up and will not change during runtime.
+ */
+public class AddSectionDataAccessObject implements AddSectionDataAccessInterface {
+    private final Workbook workbook;
+    private final CourseDataRepository courseDataRepository;
+
+    public AddSectionDataAccessObject(CourseDataRepository courseDataRepository,
+                                      Workbook workbook) {
+        this.workbook = workbook;
+        this.courseDataRepository = courseDataRepository;
+    }
+
+    @Override
+    public Workbook getWorkbook() {
+        return workbook;
+    }
+
+
+    @Override
+    public Section getSectionToAdd(String courseOfferingIdentifier, String sectionName) throws IllegalArgumentException {
+        CourseOffering courseOfferingToAdd = courseDataRepository.getCourseOffering(courseOfferingIdentifier);
+
+        // sectionName is taken from our UI so should be in expected format
+        for (Section section : courseOfferingToAdd.getAvailableSections()) {
+            if (section.getName().equals(sectionName)) {
+                return section;
+            }
+        }
+        throw new IllegalArgumentException("Section " + sectionName + " not found in Course Offering "
+                + courseOfferingId);
+    }
+}
